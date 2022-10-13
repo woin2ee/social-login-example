52dba564be982b5e9d4d0392a258d5be7862d189
Jaewon <woin2ee@gmail.com>
2022-10-13 17:18:04 +0900

    프로젝트 생성

--- SocialLoginExample/Scenes/MainViewController.swift
+++ SocialLoginExample/Scenes/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SocialLoginExample
 //
 //  Created by Jaewon on 2022/10/13.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
