06f51b41ff1248b331185996376e8cd42818cba2
Jaewon <woin2ee@gmail.com>
2022-10-17 16:56:45 +0900

    카카오 로그인으로 Access, Refresh token 발급 구현

--- SocialLoginExample/Scenes/MainViewController.swift
+++ SocialLoginExample/Scenes/MainViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
 
 final class MainViewController: UIViewController {
     
@@ -15,5 +17,39 @@ final class MainViewController: UIViewController {
     
     @IBAction func didTapKakaoLogin(_ sender: UIButton) {
         print("KakaoLogin")
+        
+        if AuthApi.hasToken() {
+            UserApi.shared.accessTokenInfo { accessTokenInfo, error in
+                if let _ = error {
+                    self.loginWithKakao()
+                } else {
+                    print("AccessTokenInfo: \(String(describing: accessTokenInfo))")
+                }
+            }
+        } else {
+            loginWithKakao()
+        }
+    }
+    
+    private func loginWithKakao() {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                } else {
+                    print("Access Token 발급 성공")
+                    _ = oauthToken
+                }
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                } else {
+                    print("Access Token 발급 성공")
+                    _ = oauthToken
+                }
+            }
+        }
     }
 }
